4453f8a104718146fe7be9c51b0efc12a55da102
ZARASTILL <pracshri007@gmail.com>
2024-10-09 23:37:26 +0530

    Update diabetes_prediction_web_app.py

--- diabetes_prediction_web_app.py
+++ diabetes_prediction_web_app.py
@@ -2,15 +2,11 @@ import numpy as np
 import altair as alt
 import pickle
 import streamlit as st
-import os
 import pickle
 
 
-current_dir = os.path.dirname(__file__)
-model_path = os.path.join(current_dir, 'diabetes_model.sav')
-
 # loading the saved model
-loaded_model = pickle.load(open('diabetes_model.sav', 'rb'))
+loaded_model = pickle.load(open('C:\Users\Hp\Desktop\AI PROJECT\diabetes_model.sav', 'rb'))
 
 
 # creating a function for Prediction
@@ -25,7 +21,7 @@ def diabetes_prediction(input_data):
     # reshape the array as we are predicting for one instance
     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
-    loaded_model = pickle.load(open('diabetes_model.sav', 'rb'))
+    loaded_model = pickle.load(open('C:\Users\Hp\Desktop\AI PROJECT\diabetes_model.sav', 'rb'))
 
     prediction = loaded_model.predict(input_data_reshaped)
     print(prediction)
